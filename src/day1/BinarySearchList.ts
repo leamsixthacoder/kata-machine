114029633ba8d5ba1474f26d322644a2adf48fa4
leamsi-ciber <joseismael13vm@gmail.com>
2024-06-12 05:30:07 -0400

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,22 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
 
+    let lowestValue = 0
+    let hightestValue = haystack.length
+
+    do {
+        const midPoint = Math.floor(lowestValue + (hightestValue - lowestValue) / 2)
+        const value = haystack[midPoint];
+
+        if(value === needle) {
+            return true;
+        }else if(value > needle) {
+            hightestValue = midPoint;
+            
+        }else {
+            lowestValue = midPoint + 1;
+        }
+    } while(lowestValue < hightestValue);
+
+    return false;
+
 }
